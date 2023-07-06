c76c2cbc5f5a8ff8fd63c2ea7cbf1398725f2f30
pratikkale1 <119706086+pratikkale1@users.noreply.github.com>
2023-07-07 00:10:50 +0530

    Add files via upload

---
+++ Program5.py
@@ -0,0 +1,14 @@
+
+def main():
+
+	print("Enter the 1st Number : ")
+	iNo1 = int(input())
+
+	print("Enter 2nd Number : ")
+	iNo2 = int(input())
+
+	print("First No is "+ str(iNo1) + " second number is " + str(iNo2))
+
+
+if __name__ == "__main__":
+	main() 
